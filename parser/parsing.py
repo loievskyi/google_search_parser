e04bcb11072e4d430bab78ed9f15042f6f3b0849
loievskyi <loievskyi.slava@gmail.com>
2023-05-11 18:14:18 +0300

    Added DownloadSearchDataParsingStep

--- parser/parsing.py
+++ parser/parsing.py
@@ -16,6 +16,73 @@ class CreateSearchUrlParsingStep(SimpleParsingStep):
         """
         base_url = "https://www.google.com/search?q="
         url = base_url + input_data
+        yield DownloadSearchDataParsingStep({
+            "url": url,
+            "start_position": 1,
+        })
+
+
+class DownloadSearchDataParsingStep(SimpleParsingStep):
+    def download_data(self, url):
+        """
+        url is downloading url.
+        Returns html if success downloading
+        """
+        time.sleep(1)
+        headers = {'Accept-Encoding': 'identity'}
+        return html.unescape(session.get(url, headers=headers).text)
+
+    def get_result_data(self, block_html, position):
+        """
+        html is div html for 1 block result
+        """
+        data = dict()
+
+        try:
+            data["title"] = block_html.find("h3").get_text()
+        except Exception:
+            pass
+
+        try:
+            data["url"] = block_html.find("a").get("href", None)
+        except:
+            pass
+
+        try:
+            data["description"] = block_html.find(
+                "div", class_="kCrYT").next().find("div", class_="sCuL3").get_text()
+        except:
+            pass
+
+        data["position"] = position
+        return data
+
+    def get_next_page_url(self, page_element):
+        try:
+            return "https://google.com" + page_element.find(class_="nBDE1b G5eFlf").get("href", None)
+        except:
+            pass
+
+
+    def parse(self, input_data):
+        """
+        input_data is dict: {
+            "url": str,
+            "start_position": int,
+        }
+        """
+        url = input_data["url"]
+        html = self.download_data(url)
+        soup = bs4.BeautifulSoup(html, "lxml")
+
+        data_divs = soup.find_all(
+            "div", class_="Gx5Zad fP1Qef xpd EtOod pkphOe")
+        start_position = input_data.get("start_position", 1)
+        for position, block_html in enumerate(data_divs, start=start_position):
+            if position > 100:
+                break
+
+
 
 
 if __name__ == "__main__":
